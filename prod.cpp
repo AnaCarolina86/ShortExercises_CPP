2fe0ccc82b7f0605190fea44d1e6d1a837ad958f
Ana Carolina Mendes <anaccm86@gmail.com>
2019-07-24 13:33:34 +0200

    Update prod.cpp

--- prod.cpp
+++ prod.cpp
@@ -7,8 +7,8 @@ int main(){
 int numb1;
 int numb2;
 
-	cin>>numb1; //le o primeiro numero
-	cin>>numb2; //le o segundo numero
+	cin>>numb1; //read the first number
+	cin>>numb2; //read the second number
 	
 	cout<< "PROD = " << numb1*numb2 << "\n" << endl;
 }
