e14304331c46b5be914b46688e2f9ecab1ff5621
Ana Carolina Mendes <anaccm86@gmail.com>
2019-07-24 13:19:40 +0200

    Create soma.cpp

---
+++ soma.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main(){
+ 
+int number1;
+int number2;
+int sum;
+
+	cin>>number1; //le o primeiro numero
+	cin>>number2;
+	
+	sum = number1 + number2;
+	cout << "SOMA = "<< sum << endl;
+}
