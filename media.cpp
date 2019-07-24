50e0d75d9d2f841a9043fc02faa844d5ed2d6e5a
Ana Carolina Mendes <anaccm86@gmail.com>
2019-07-24 13:33:27 +0200

    Update media.cpp

--- media.cpp
+++ media.cpp
@@ -11,8 +11,8 @@ cout << fixed << setprecision(5);
 double nota1;
 double nota2;
 
-	cin>>nota1; //leio nota 1
-	cin>>nota2; //leio nota 2
+	cin>>nota1; //read nota 1
+	cin>>nota2; //read nota 2
 	
 	cout<< "MEDIA = " << (nota1*3.5 + nota2*7.5)/11 << endl;
 }
