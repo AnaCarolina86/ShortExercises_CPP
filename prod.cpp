e1ab394e35f6c1e4a5b6f54d73a40f04b0f61270
Ana Carolina Mendes <anaccm86@gmail.com>
2019-07-24 13:19:14 +0200

    Create prod.cpp

---
+++ prod.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+int numb1;
+int numb2;
+
+	cin>>numb1; //le o primeiro numero
+	cin>>numb2; //le o segundo numero
+	
+	cout<< "PROD = " << numb1*numb2 << "\n" << endl;
+}
